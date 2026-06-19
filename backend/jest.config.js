cfc5e952f3c0285fa941330a4cfff5245fa2902d
James He <jamesheden@Jamesdebijibendiannao.local>
2026-06-18 22:00:16 -0400

    fix jest transform for delivery client

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: '.',
   testRegex: '.*\\.spec\\.ts$',
-  transform: { '^.+\\.(t|j)s$': 'ts-jest' },
+  transform: { '^.+\\.ts$': 'ts-jest' },
   collectCoverageFrom: ['src/**/*.(t|j)s'],
   coverageDirectory: './coverage',
   testEnvironment: 'node',
