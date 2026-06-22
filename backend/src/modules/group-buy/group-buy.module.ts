f0c2f8a4b618af82670cf8bce463cbd2fa3f3ee7
James He <jamesheden@Jamesdebijibendiannao.local>
2026-06-22 00:11:53 -0400

    feat: add cash-only group buy checkout

--- backend/src/modules/group-buy/group-buy.module.ts
+++ backend/src/modules/group-buy/group-buy.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
+import { ModuleRef } from '@nestjs/core';
 
 import { GroupBuyCheckoutService } from './group-buy-checkout.service';
 import { GroupBuyController } from './group-buy.controller';
 import { GroupBuyLifecycleService } from './group-buy-lifecycle.service';
 import { GroupBuyRebateService } from './group-buy-rebate.service';
 import { GroupBuyService } from './group-buy.service';
+import { AlipayService } from '../payment/alipay.service';
+import { WechatPayService } from '../payment/wechat-pay.service';
 
 @Module({
   controllers: [GroupBuyController],
@@ -16,4 +19,21 @@ import { GroupBuyService } from './group-buy.service';
   ],
   exports: [GroupBuyCheckoutService, GroupBuyLifecycleService, GroupBuyRebateService],
 })
-export class GroupBuyModule {}
+export class GroupBuyModule implements OnModuleInit {
+  constructor(
+    private readonly moduleRef: ModuleRef,
+    private readonly checkoutService: GroupBuyCheckoutService,
+  ) {}
+
+  onModuleInit() {
+    const alipayService = this.moduleRef.get(AlipayService, { strict: false });
+    if (alipayService) {
+      this.checkoutService.setAlipayService(alipayService);
+    }
+
+    const wechatPayService = this.moduleRef.get(WechatPayService, { strict: false });
+    if (wechatPayService) {
+      this.checkoutService.setWechatPayService(wechatPayService);
+    }
+  }
+}
